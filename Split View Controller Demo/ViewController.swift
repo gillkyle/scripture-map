b380271efa7b171b6d127a92d5eab6654064e1e8
Steve Liddle <liddle@byu.edu>
2018-11-01 11:09:57 -0600

    Initial Commit

---
+++ Split View Controller Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Split View Controller Demo
+//
+//  Created by Steve Liddle on 11/1/18.
+//  Copyright © 2018 IS 543. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
